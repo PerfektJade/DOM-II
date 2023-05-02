78f9c73900facdb64d4ba44ced961085b888a199
Jade Coonts <perfektjade@gmail.com>
2023-05-02 13:55:46 -0400

    1st Commit, 4/10 Events Created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './less/index.less'
 
-// Your code goes here!
+// CREATE 10 EVENT LISTENERS
+//
+// 1 - load event
+window.onload = function (evt) {
+    console.log(`Event ${evt.type} fired! Ready to go!`)
+    const heading = document.querySelector('h1')
+    heading.textContent = 'READY TO GO!!'
+// 2 - copy event
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+            .then(text => {
+                heading.textContent += text
+            })
+    })
+// 3 - click event
+    document.body.addEventListener('click', evt => {
+            evt.target.classList.toggle('mirror')
+    })
+// 4 - dblclick event
+    document.body.addEventListener('dblclick', evt => {
+        evt.target.outerHTML = ''
+    })
+// 5 - event
+    
+
+}
+
+// 
+// 
+// USE 'preventDefault'
