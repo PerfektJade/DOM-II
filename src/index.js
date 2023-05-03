66566101d2c8029521b5488f28cabb7dbb66fae5
Jade Coonts <perfektjade@gmail.com>
2023-05-02 20:40:04 -0400

    4th Commit, COMPLETE

--- src/index.js
+++ src/index.js
@@ -33,23 +33,37 @@ window.onload = function (evt) {
         const {clientX, clientY} = evt
         // console.log(`Mouse is at ${clientX}, ${clientY}`)
     })
-// 7 - mouseenter 
+// 7 & 8 - mouseenter & mouseleave
     const destinations = document.querySelectorAll('.destination')
     for (let destination of destinations) {
         destination.addEventListener('mouseenter', () => {
             destination.style.fontWeight = 'bold'
         })
-// 8 - mouseleave
         destination.addEventListener('mouseleave', () => {
             setTimeout(() => {
                 destination.style.fontWeight = 'initial'
             }, 500)
         })
     }
-// 9 - 
-// 10 - 
-}
-
-// 
-// 
 // USE 'preventDefault'
+// 9 - wheel
+    function zoom(evt) {
+        evt.preventDefault();
+        scale += evt.deltaY * -0.01;
+        scale = Math.min(Math.max(0.125, scale), 4);
+        el.style.transform = `scale(${scale})`;
+    }
+    let scale = 1;
+    const el = document.querySelector('img');
+    el.onwheel = zoom;
+// 10 - mouseover
+    const btns = document.querySelectorAll('.btn');
+    for (let btn of btns) {
+        btn.addEventListener('mouseover', () => {
+            btn.style.color = 'orange';
+            setTimeout(() => {
+                btn.style.color = ''
+            }, 500);
+        }, false);
+    };
+};
